ce3e53a004894416da082c56c937f59bcd7cb844
alealfonsi <a.alfonsi1999@gmail.com>
2024-04-23 20:20:03 +0200

    completed the reception of the data exchange telegram, started the handler for the response

--- pyprofibus/slave/Data_ExchState.py
+++ pyprofibus/slave/Data_ExchState.py
@@ -1,3 +1,4 @@
+from pyprofibus.dp.dp import DpTelegram_DataExchange_Con, DpTelegram_DataExchange_Req
 from pyprofibus.fieldbus_data_link.fdl import FdlTelegram, FdlTelegram_var
 from pyprofibus.slave.Slave import Slave, SlaveException
 from pyprofibus.slave.SlaveState import SlaveState
@@ -6,18 +7,42 @@ class Data_ExchState(SlaveState):
 
     def __checkTelegram(self, telegram):
             if FdlTelegram_var.checkType(telegram):
-                """slave.fcb.handleReply()"""
+                self.__checkDpTelegram(telegram)
             elif telegram.da == FdlTelegram.ADDRESS_MCAST:
                 #TO-DO
                 #self.__handleMcastTelegram(telegram)
                 """"""
             elif 0:
                 #TO-DO
-                 """Whatever other kind of frame type accepted in this state...TO-DO later"""
+                 """Whatever other kind of fdl frame type accepted in this state...TO-DO later"""
             else:
                  raise SlaveException("Slave " + str(self.getSlave().getId()) + """ is in 
-                                      Data Exchange state and can't accept this type of telegram.
+                                      Data Exchange state and can't accept this type of telegram.\n
                                       Telegram: %s""" % str(telegram))
+    
+    def __checkDpTelegram(self, telegram):
+        #check if something with field check bit is important before this step
+        if DpTelegram_DataExchange_Req.checkType(telegram):
+            self.__handleDataExchangeRequest(telegram)
+        elif 0:
+             #TO-DO
+             """Whatever other kind of dp frame accepted in this state...TO-DO later"""
+        else:
+             raise SlaveException("Slave " + str(self.getSlave().getId()) + """ is in 
+                                      Data Exchange state and received a proper telegram at the fdl level 
+                                    but not handled at the dp level (either wrong or not yet handled by the library)\n
+                                      Telegram: %s""" % str(telegram))
+        
+    def __handleDataExchangeRequest(self, telegram):
+        # Here the handling of the meaning of the telegram has to be done
+        # For now, this method is implemented just answering to the master with a standard frame
+        # of the proper type
+        response_telegram = DpTelegram_DataExchange_Con(da = self.getSlave().getMasterAddress(),
+                                                        sa = self.getAddress(),
+                                                        fc = FdlTelegram.FC_DL,
+                                                        du = b"\x00\x00\x00\x00")
+        #TO-DO send the telegram
+        
         
     def setParameters(self, watchdog, slave_reaction_time, freeze_mode_enable, locked, group, master_add, id):
         raise SlaveException("Slave " + str(self.getSlave().getId()) + " is in Data Exchange state, can't accept parameterization!")
