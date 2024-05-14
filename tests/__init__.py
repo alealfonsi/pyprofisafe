f301cde26a5fa7bd863c2ee6face01947849dac8
alealfonsi <a.alfonsi1999@gmail.com>
2024-05-14 21:03:39 +0200

    tests for data exchange telegram

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,2 +1,3 @@
 from test_dummy import *
 from test_gsd import *
+from test_pyprofibus import *
---
+++ tests/test_profibus.py
@@ -0,0 +1,45 @@
+from __future__ import division, absolute_import, print_function, unicode_literals
+
+import sys
+sys.path.insert(0, "/home/alessio/pyprofisafe")
+
+from pyprofibus import *
+
+from pyprofibus.dp.dp import DpTelegram_DataExchange_Con
+from pyprofibus.physical.phy_serial import CpPhySerial
+from pyprofibus.slave.Data_ExchState import Data_ExchState
+from pyprofibus.slave.ResetState import ResetState
+from pyprofibus.slave.Slave import Slave
+from pyprofibus_tstlib import *
+initTest(__file__)
+
+import pyprofibus
+import pyprofibus.conf
+import pyprofibus.dp
+import pyprofibus.master
+import pyprofibus.physical
+import pyprofibus.slave
+
+class Test_MasterSlaveProfibus():
+    
+    def test_DataExchangeTelegram():
+        #init
+        phy = CpPhySerial("/dev/ttyS0", True)
+        slave = Slave(phy)
+        slave.setState(ResetState())
+        #slave.setState(Wait_PrmState(slave))
+        #parameterization
+        slave.setAddress(0)
+        slave.setParameters(600000, 100, False, False, 0, 111, "first")
+        slave.setState(Data_ExchState())
+        
+        out_du = bytearray()
+        
+        send_telegram = DpTelegram_DataExchange_Con(
+                     slave.getMasterAddress(),
+                     slave.address,
+                     slave.rx_telegram.fc,
+                     out_du
+                )
+        print("Built telegram: %s" % send_telegram)
+        assert(DpTelegram_DataExchange_Con.checkType(send_telegram))
